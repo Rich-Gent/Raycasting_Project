a80690339e3f246ef5315480048845d076724483
Rich-Gent <rich.m.gent@gmail.com>
2023-12-22 12:08:21 +0000

    Start of the project

---
+++ raycasting/raycast.js
@@ -0,0 +1,126 @@
+const TILE_SIZE = 32;
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15;
+
+const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+
+class Map {
+    constructor() {
+        this.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+    }
+    hasWallAt(x, y){
+        if(x<0 || x>WINDOW_WIDTH || y<0 || y>WINDOW_HEIGHT){
+            return true;
+        }
+        var mapGridIndexX = Math.floor(x / TILE_SIZE);
+        var mapGridIndexY = Math.floor(y / TILE_SIZE);
+        return this.grid[mapGridIndexY][mapGridIndexX] !=0 ;
+    }
+    render() {
+        for (var i = 0; i < MAP_NUM_ROWS; i++) {
+            for (var j = 0; j < MAP_NUM_COLS; j++) {
+                var tileX = j * TILE_SIZE; 
+                var tileY = i * TILE_SIZE;
+                var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
+                stroke("#222");
+                fill(tileColor);
+                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+            }
+        }
+    }
+}
+
+class Player{
+    constructor(){
+        this.x = WINDOW_WIDTH/2;
+        this.y = WINDOW_HEIGHT/2;
+        this.radius = 3;
+        this.turnDirection = 0; //-1 if left, +1 if right
+        this.walkDirection = 0 ; //-1 if back, +1 if front
+        this.rotationAngle = Math.PI/2;
+        this.moveSpeed = 2.0; //pixels per frame
+        this.rotationSpeed = 2*(Math.PI/180) //converting to radians
+    }
+    update(){
+        
+        //update player position based on turn and walk direction
+        this.rotationAngle += this.rotationSpeed*this.turnDirection
+        //how much to move per frame
+        var moveStep = this.walkDirection * this.moveSpeed
+        var newPlayerX = this.x + Math.cos(this.rotationAngle)*moveStep
+        var newPlayerY = this.y + Math.sin(this.rotationAngle)*moveStep
+        //only set new player position if not colliding with the map walls
+        if(!grid.hasWallAt(newPlayerX,newPlayerY)){
+            this.x=newPlayerX
+            this.y=newPlayerY
+        }
+    }
+    render(){
+        noStroke();
+        fill("red");
+        circle(this.x,this.y, this.radius);
+        stroke("red");
+        //draw line to see which way we are facing
+        line(this.x,this.y, this.x+Math.cos(this.rotationAngle)*20,this.y+ Math.sin(this.rotationAngle)*20);
+    }
+}
+
+var grid = new Map();
+var player = new Player();
+
+function keyPressed(){
+    if (keyCode == UP_ARROW){
+        player.walkDirection = 1;
+    }else if (keyCode == DOWN_ARROW){
+        player.walkDirection = -1;
+    }else if (keyCode == RIGHT_ARROW){
+        player.turnDirection = 1;
+    }else if (keyCode == LEFT_ARROW){
+        player.turnDirection = -1;
+    }
+}
+
+function keyReleased(){
+    if (keyCode == UP_ARROW){
+        player.walkDirection = 0;
+    }else if (keyCode == DOWN_ARROW){
+        player.walkDirection = 0;
+    }else if (keyCode == RIGHT_ARROW){
+        player.turnDirection = 0;
+    }else if (keyCode == LEFT_ARROW){
+        player.turnDirection = 0;
+    }
+}
+
+
+function setup() {
+    //initialise all objects
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+}
+
+function update() {
+    //update all game objects before we render the next frame
+    player.update();
+}
+
+function draw() {
+    //render all objects frame by frame
+    update();
+
+    grid.render();
+    player.render();
+}
